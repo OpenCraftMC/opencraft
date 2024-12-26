6f0f03072b81adb80e09f12928eb290f0f5c396f
chyves <82107846+notchyves@users.noreply.github.com>
2024-12-26 19:23:29 +0000

    go

--- src/main.cpp
+++ src/main.cpp
@@ -1,84 +1,25 @@
-#include <GLES2/gl2.h>
+#include <bgfx/bgfx.h>
+#include <bgfx/platform.h>
+#include <GL/glut.h>
 
-#ifdef EMSCRIPTEN
-#include <emscripten.h>
-#endif
-
-const char* vertexShaderSource = R"(
-    attribute vec4 a_Position;
-    void main() {
-        gl_Position = a_Position;
-    }
-)";
-
-const char* fragmentShaderSource = R"(
-    precision mediump float;
-    uniform vec4 u_Color;
-    void main() {
-        gl_FragColor = u_Color;
-    }
-)";
-
-GLuint shaderProgram;
-GLuint positionAttr;
-GLuint colorUniform;
-GLuint vertexBuffer;
-
-void initOpenGL() {
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
-    glCompileShader(vertexShader);
-    
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
-    glCompileShader(fragmentShader);
-    
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glUseProgram(shaderProgram);
-    
-    positionAttr = glGetAttribLocation(shaderProgram, "a_Position");
-    colorUniform = glGetUniformLocation(shaderProgram, "u_Color");
-
-    GLfloat vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.5f, 0.5f, 0.0f,
-        -0.5f, 0.5f, 0.0f
-    };
-    
-    glGenBuffers(1, &vertexBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-}
-
-void renderFrame() {
+void display() {
+    bgfx::renderFrame();
     glClear(GL_COLOR_BUFFER_BIT);
-    glUseProgram(shaderProgram);
-    glEnableVertexAttribArray(positionAttr);
-    glVertexAttribPointer(positionAttr, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-    glUniform4f(colorUniform, 0.0f, 1.0f, 0.0f, 1.0f);
-    glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-    glDisableVertexAttribArray(positionAttr);
+    glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5, -0.5);
+        glVertex2f(0.5, -0.5);
+        glVertex2f(0.0, 0.5);
+    glEnd();
+    glFlush();
 }
 
-int main() {
-    #ifdef EMSCRIPTEN
-        emscripten_set_canvas_size(800, 600);
-    #endif
-
-    glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
-    
-    initOpenGL();
-
-#ifdef EMSCRIPTEN
-    emscripten_set_main_loop(renderFrame, 0, 1);
-#else
-    while (true) {
-        renderFrame();
-    }
-#endif
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutCreateWindow("Multi-Platform Triangle");
+    glutDisplayFunc(display);
+    bgfx::init();
+    bgfx::reset(800, 600);
+    glutMainLoop();
+    bgfx::shutdown();
     return 0;
 }
